624ae8833b8579ce1c9c1b516f07d6f58bd8ffc8
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-07-17 16:33:57 -0700

    Create basic JS confirm password validation

---
+++ script.js
@@ -0,0 +1,40 @@
+const passwordField = document.querySelector("#pwd");
+const confirmPasswordField = document.querySelector("#c-pwd");
+const confirmPasswordText = document.querySelector(".confirm-password");
+const submitButton = document.querySelector("button");
+
+function checkPassword() {
+    let password = passwordField.value;
+    let confirmPassword = confirmPasswordField.value;
+    if (password !== confirmPassword) {
+        confirmPasswordText.textContent = "Passwords do not match";
+        submitButton.disabled = true;
+        confirmPasswordText.style.display="inline";
+        confirmPasswordField.style.backgroundColor = "rgba(255, 0, 0, 0.411)";
+        confirmPasswordField.style.borderColor = "rgb(155, 58, 58)";
+        if (confirmPassword === "" && password === "") {
+            confirmPasswordField.style.backgroundColor = "white";
+            confirmPasswordField.style.borderColor = "#AAA";
+        }
+    }
+    else {
+        confirmPasswordText.textContent = "";
+        submitButton.disabled = false;
+        confirmPasswordText.style.display="none";
+        confirmPasswordField.style.backgroundColor = "rgba(50, 177, 0, 0.336)";
+        confirmPasswordField.style.borderColor = "#266417";
+        if (confirmPassword === "" && password === "") {
+            confirmPasswordField.style.backgroundColor = "white";
+            confirmPasswordField.style.borderColor = "#AAA";
+            passwordField.style.backgroundColor = "white";
+            passwordField.style.borderColor = "#AAA";
+        }
+    }
+    if (password.length < 6 && password !== "" && confirmPassword !== "") {
+        passwordField.style.backgroundColor = "rgba(255, 0, 0, 0.411)";
+        passwordField.style.borderColor = "rgb(155, 58, 58)";
+        confirmPasswordField.style.backgroundColor = "rgba(255, 0, 0, 0.411)";
+        confirmPasswordField.style.borderColor = "rgb(155, 58, 58)";
+    }
+
+}
